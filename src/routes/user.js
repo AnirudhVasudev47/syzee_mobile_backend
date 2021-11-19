4c4f52148b2e282bf8e77f1685aaba8af20b4fa1
Anirudh <anirudh040799@gmail.com>
2021-11-19 16:20:02 +0530

    Initial Commit

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const {
-  test, homeTabBanner, homeTabCategories, homeTabBrands, homeTabMostWanted, homeTabMostPopular, signup, checkUser
+  test, signup, checkUser
 } = require("../controllers/user")
 
 // test
